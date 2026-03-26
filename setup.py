ace7dab2ef343e4a93e8875e2938205b60ab316b
Arjun Chaurasiya <arjunch0367@gmail.com>
2026-03-26 23:36:21 +0530

    Requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Arjun Chaurasiya",
+    author_email="arjunch0367@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
